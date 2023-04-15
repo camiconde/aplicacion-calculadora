f350e96cd6fb228bf524f6ddff91e2cc9f2ec1d8
camila <emacamilaconde@gmail.com>
2023-04-15 11:42:23 -0300

    agregue archivos

---
+++ funciones/division.js
@@ -0,0 +1,7 @@
+function division(numero1, numero2) {
+    if (numero2 == 0) {
+        return 'no se puede realizar la operacion';
+    }
+    return numero1 / numero2
+
+} module.exports = { division }
